c39c8555a4bfe249d21d4ef4cd3e83ad6021d4f6
Phillip Shreves <hi@phillips.work>
2019-03-27 23:31:12 -0500

    added gitignore

---
+++ hello_cargo/target/debug/deps/hello_cargo-8e157420591bc5ec.d
@@ -0,0 +1,5 @@
+/home/phillip/projects/rust-learning/hello_cargo/target/debug/deps/hello_cargo-8e157420591bc5ec: src/main.rs
+
+/home/phillip/projects/rust-learning/hello_cargo/target/debug/deps/hello_cargo-8e157420591bc5ec.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/phillip/projects/rust-learning/hello_cargo/target/debug/hello_cargo: /home/phillip/projects/rust-learning/hello_cargo/src/main.rs
