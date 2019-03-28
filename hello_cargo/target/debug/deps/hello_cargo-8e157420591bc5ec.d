0e4114aa57aac9503d4100e31bb6a7392cc52ac5
Phillip Shreves <hi@phillips.work>
2019-03-27 23:35:53 -0500

    removed target folder

--- hello_cargo/target/debug/deps/hello_cargo-8e157420591bc5ec.d
+++
@@ -1,5 +0,0 @@
-/home/phillip/projects/rust-learning/hello_cargo/target/debug/deps/hello_cargo-8e157420591bc5ec: src/main.rs
-
-/home/phillip/projects/rust-learning/hello_cargo/target/debug/deps/hello_cargo-8e157420591bc5ec.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/phillip/projects/rust-learning/hello_cargo/target/debug/hello_cargo: /home/phillip/projects/rust-learning/hello_cargo/src/main.rs
