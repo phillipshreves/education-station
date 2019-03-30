0931c2f7441a84e79871eee466e17dbb7b888944
Phillip Shreves <hi@phillips.work>
2019-03-29 21:51:43 -0500

    saving changes

--- misc_binaries/functions/src/main.rs
+++ misc_binaries/functions/src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
     another_function(5, 6);
+
+    let five = plus_one(five());
+
+    println!("The variable five is {}", five);
 }
 
 fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(input: i32) -> i32 {
+    input + 1
 }
