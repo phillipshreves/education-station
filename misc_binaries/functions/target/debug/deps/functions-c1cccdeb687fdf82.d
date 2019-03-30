817e9eb90b962d871d59a0b3155c219127341b2d
Phillip Shreves <hi@phillips.work>
2019-03-29 21:51:08 -0500

    Saving work

--- misc_binaries/functions/target/debug/deps/functions-c1cccdeb687fdf82.d
+++
@@ -1,5 +0,0 @@
-/home/phillip/projects/rust-learning/misc_binaries/functions/target/debug/deps/functions-c1cccdeb687fdf82: src/main.rs
-
-/home/phillip/projects/rust-learning/misc_binaries/functions/target/debug/deps/functions-c1cccdeb687fdf82.d: src/main.rs
-
-src/main.rs:
--- misc_binaries/functions/target/debug/functions.d
+++
@@ -1,1 +0,0 @@
-/home/phillip/projects/rust-learning/misc_binaries/functions/target/debug/functions: /home/phillip/projects/rust-learning/misc_binaries/functions/src/main.rs
