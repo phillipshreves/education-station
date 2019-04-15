c7afe7476aacdca4e6e5da3484f09283ffadc2eb
Phillip Shreves <pshreves@highpointgo.com>
2019-04-15 12:59:22 -0500

    finished rectangle binary

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,7 +1,21 @@
-fn main() {
-    let width1 = 30;
-    let height1 = 50;
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-    println!(");
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!(
+        "The area of the rectagle is {} square pixels.",
+        area(&rect1)
+    );
+
+    println!("rect1 is {:#?}", rect1);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
 
