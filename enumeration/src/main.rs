115e72276cbb2d3283b8bd49eaf1d66a3b4100d7
Phillip Shreves <pshreves@highpointgo.com>
2019-04-24 12:26:45 -0500

    updated enumeration binary

--- enumeration/src/main.rs
+++ enumeration/src/main.rs
@@ -12,5 +12,22 @@ fn main() {
     let home = IpAddress::V4(127, 0, 0, 1);
 
     let loopback = IpAddress::V6(String::from("::1"));
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn call(&self) {
+        }
+
+    }
+    
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
 }
 
