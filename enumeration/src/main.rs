80ef5af44eae858f0722d678d10a89cd65a694af
Phillip Shreves <pshreves@highpointgo.com>
2019-04-24 13:02:58 -0500

    updated enumeration binary

--- enumeration/src/main.rs
+++ enumeration/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    //enumeration setup
     enum IpAddressKind {
         V4,
         V6,
@@ -12,6 +13,8 @@ fn main() {
     let home = IpAddress::V4(127, 0, 0, 1);
 
     let loopback = IpAddress::V6(String::from("::1"));
+
+    //Methods
 
     enum Message {
         Quit,
@@ -29,5 +32,44 @@ fn main() {
     let m = Message::Write(String::from("hello"));
     m.call();
 
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+    //Match control flow operator
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska,
+        California,
+        Texas,
+    }
+
+    enum Coin {
+        Penny, 
+        Nickel, 
+        Dime, 
+        Quarter(UsState),
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky penny!");
+                1
+            },
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => {
+                println!("State quarter from {:?}!", state);
+                25
+            },
+        }
+    }
+
+            value_in_cents(
+                Coin::Quarter(UsState::Alaska)
+            ) 
 }
 
