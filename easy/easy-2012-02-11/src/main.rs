387304eb33d6374944e1cecf14281efeb8d34dc7
Phillip Shreves <hi@phillips.work>
2019-06-10 18:13:33 -0500

    finished binary for encryption

--- easy/easy-2012-02-11/src/main.rs
+++ easy/easy-2012-02-11/src/main.rs
@@ -3,25 +3,30 @@ fn get_input() -> String {
     let mut input = String::new();
     match std::io::stdin().read_line(&mut input) {
         Ok(string) => input,
-        Err(error) => String::from("")
+        Err(error) => String::new()
     }
 }
 
-fn encrypt(message: String) -> String {
-    for character in message.chars() {
-        let character_new = std::char::from_u32(character as u32 - 13);
-        let mut encrypted: String = "";
-        //encrypted.push(character_new);
-        match
+fn encrypt(message: &String) -> String {
+    let mut encrypted = String::new();
+    for char in message.chars() {
+        let char_number = char as u8;
+        let char_encryption = if char_number + 13 > 122 {
+            char_number + 13 - 57
+        } else {
+            char_number + 13
+        };
+        let char_new = char_encryption as char;
+        match char_encryption {
+            65..=122_ => encrypted.push_str(&char_new.to_string()),
+            _ => ()
+        }
     }
+    encrypted
 }
 
 fn main() {
     let message = get_input();
-    if message.is_empty() {
-        println!("Cannot encrypt empty message");
-    } else {
-        let encrypted_message = encrypt(message);
-        println!("Encrypted message: {}", encrypted_message);
-    }
+    let encrypted_message = encrypt(&message);
+    println!("Encrypted message: {}", encrypted_message);
 }
