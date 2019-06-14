2532d13fd0faf84a70ea7b012e2b8d99504cf871
Phillip Shreves <hi@phillips.work>
2019-06-14 18:17:50 -0500

    Added challenge Goal in comments at beginning of binaries

--- easy/easy-2012-02-11/src/main.rs
+++ easy/easy-2012-02-11/src/main.rs
@@ -1,3 +1,11 @@
+/*Challenge #3 [easy]
+ * Welcome to cipher day!
+ *
+ * write a program that can encrypt texts with an alphabetical caesar cipher. This cipher can
+ * ignore numbers, symbols, and whitespace.
+ *
+ * for extra credit, add a "decrypt" function to your program!
+ * */
 
 fn get_input() -> String {
     let mut input = String::new();
--- easy/easy-2012-02-12/src/main.rs
+++ easy/easy-2012-02-12/src/main.rs
@@ -1,3 +1,12 @@
+/*Challenge #4 [easy]
+ * You're challenge for today is to create a random password generator!
+ *
+ * For extra credit, allow the user to specify the amount of passwords to generate.
+ *
+ * For even more extra credit, allow the user to specify the length of the strings he wants to
+ * generate!
+ * */
+
 
 extern crate rand;
 use rand::prelude::*;
