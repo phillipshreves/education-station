7bda838a363a60b8eaec0eb3f05a9c8765f0af9e
Phillip Shreves <hi@phillips.work>
2020-01-01 15:50:06 -0600

    Fixed recusive bug

--- 2019/day-1/src/main.rs
+++ 2019/day-1/src/main.rs
@@ -5,12 +5,16 @@ fn main() {
 
     let masses: Vec<i64>= data.split("\n").filter_map(|w| w.parse().ok()).collect();
 
+    let mut planetary_fuel: i64 = 0;
     let mut total_fuel: i64 = 0;
     for mass in masses { 
-       total_fuel = total_fuel + mass_to_fuel(mass); 
+        let fuel = mass_to_fuel(mass);
+        planetary_fuel = planetary_fuel + fuel; 
+        total_fuel = total_fuel + fuel_compensation(fuel);
+
     };
 
-    println!("{}", total_fuel);
+    println!("Fuel for planetary travel: {}\nFuel for total travel:{}", planetary_fuel, total_fuel);
 }
 
 fn read_file(filename: &str) -> String {
@@ -22,4 +26,15 @@ fn read_file(filename: &str) -> String {
 
 fn mass_to_fuel(mass: i64) -> i64{
    (mass/3) - 2
+}
+
+fn fuel_compensation(fuel: i64) -> i64{
+    if fuel < 1 {
+        return 0
+    };
+
+    fuel + fuel_compensation (
+        mass_to_fuel(fuel)
+    )
+
 }
