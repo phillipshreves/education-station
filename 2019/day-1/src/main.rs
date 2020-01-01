17e433dc7f87444a2925bd574abaf11640c27a73
Phillip Shreves <hi@phillips.work>
2020-01-01 15:14:54 -0600

    Finished day 1 binary

--- 2019/day-1/src/main.rs
+++ 2019/day-1/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let data: String = read_file("input.txt");
+
+    let masses: Vec<i64>= data.split("\n").filter_map(|w| w.parse().ok()).collect();
+
+    let mut total_fuel: i64 = 0;
+    for mass in masses { 
+       total_fuel = total_fuel + mass_to_fuel(mass); 
+    };
+
+    println!("{}", total_fuel);
+}
+
+fn read_file(filename: &str) -> String {
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    contents
+}
+
+fn mass_to_fuel(mass: i64) -> i64{
+   (mass/3) - 2
 }
