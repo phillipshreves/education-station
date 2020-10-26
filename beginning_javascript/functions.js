3c3c1f9f934636d92817825c8d9e3fefa94c8d92
Phillip Shreves <hi@phillips.work>
2020-10-25 20:18:43 -0500

    Updated repositories, added Beginning Javascript

---
+++ beginning_javascript/functions.js
@@ -0,0 +1,7 @@
+function writeUserWelcome(userName, userAge) {
+	document.write("Welcome to my website " + userName + "<br />");
+	document.write("Hope you enjoy it!");
+	document.write("Your age is " + userAge ) ;
+}
+
+writeUserWelcome("Phil", 26);
